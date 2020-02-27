fff1f831da916b53e6f642beb069f34b5c7f876a
timkinsman <tkinsm@gmail.com>
2020-02-28 01:03:42 +1030

    config file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const clientId = "18260e6b36504ea4bbfeab85441acf83";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+    "user-top-read",
+    "user-readprivate"
+];
