a973e7ea89295c9bf3f939641336c33abeacb7a9
timkinsman <tkinsm@gmail.com>
2020-02-28 13:47:40 +1030

    axios spotify

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "18260e6b36504ea4bbfeab85441acf83";
 export const redirectUri = "http://localhost:3000/";
 export const scopes = [
-    "user-top-read",
-    "user-read-private"
+    "user-read-private",
+    "user-top-read"
 ];
