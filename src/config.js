30731297b963109ebb6298196881eb49cb187d87
timkinsman <tkinsm@gmail.com>
2020-03-19 16:17:44 +1000

    heroku fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "18260e6b36504ea4bbfeab85441acf83";
-export const redirectUri = "https://localhost:3000/";
+export const redirectUri = "https://topify-app.herokuapp.com/";
 export const scopes = [
     "user-read-private",
     "user-top-read"
