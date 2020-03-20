10a1f0c6a70c2a47f94276aadbcb28990adc1a3d
timkinsman <tkinsm@gmail.com>
2020-03-20 13:46:06 +1000

    loading bar fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "18260e6b36504ea4bbfeab85441acf83";
-export const redirectUri = "https://topify-app.herokuapp.com/";
+export const redirectUri = "http://localhost:3000/";
 export const scopes = [
     "user-read-private",
     "user-top-read"
