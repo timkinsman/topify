f77925245c56deac56be29554a0659ff642bfdd9
timkinsman <tkinsm@gmail.com>
2020-03-24 00:05:45 +1000

    marginBottom fix

--- src/components/ToptifyTop.js
+++ src/components/ToptifyTop.js
@@ -12,7 +12,7 @@ const TopifyTop = ({myTopArtists, spotifyApi}) => {
         <div className="ui stackable one column grid" style={{padding: '100px 0', height: '100vh'}}>
             <div className="middle aligned column">
                 <h1 className="ui inverted grey header" style={{fontSize: '-webkit-xxx-large', textAlign: 'center'}}>YOUR TOP <a className={type === 'artists' ? null : "not-active"} onClick={() => setType('artists')}>ARTISTS</a> <a className={type === 'tracks' ? null : "not-active"} onClick={() => setType('tracks')}>TRACKS</a></h1>
-                <div style={{display: 'flex', justifyContent: 'center', marginBottom: '64px'}}>
+                <div style={{display: 'flex', justifyContent: 'center', marginBottom: '14px'}}>
                     <a className={timeRange === 'lifetime' ? null : "not-active"} onClick={() => setTimeRange('lifetime')} style={{margin: '0 10px 14px 10px'}}>lifetime</a>
                     <a className={timeRange === 'recent' ? null : "not-active"} onClick={() => setTimeRange('recent')} style={{margin: '0 10px 14px 10px'}}>recent listening</a>
                 </div>
